05ee4224171cda1a35ab661cd5424d4caadced21
Semih Çalışkan <dtsemihcaliskan@gmail.com>
2021-11-02 00:46:03 +0300

    öğrenmeye çalışıyom

---
+++ helloworld/src/Helloworld.java
@@ -0,0 +1,11 @@
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Tuncay");
+        int myfirstnumber = 5 + 25;
+        int mysecondenumber = 75;
+        int mytotal = (myfirstnumber) + (mysecondenumber);
+        System.out.println("toplam" + (mytotal));
+
+
+    }
+}
